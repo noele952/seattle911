94929bbac80b283bc86bd3f2f112cff88d9f8a81
Noel <noele952@gmail.com>
2022-09-07 09:54:06 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+from funcs import *
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField, StringField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+import os
+import boto3
+
+# dynamodb = boto3.resource('dynamodb')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "SECRET_KEY"
+Bootstrap(app)
+sns = boto3.resource('sns')
+topic = 'arn:aws:sns:us-east-1:643020469822:Seattle-911-message'
+
+
+df = pd.read_csv('static/df_72hr.csv')
+geojson_df = create_geojson_df_csv('static/seattle_neighborhoods.geojson')
+neighborhood_list = create_neighborhood_list(geojson_df)
+
+
+class CreateMapForm(FlaskForm):
+    incident = SelectField('Incident Type', choices=get_incident_list())
+    neighborhood = SelectField('Neighborhood', choices=neighborhood_list)
+    submit = SubmitField("Submit")
+
+
+class CreateContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    message = StringField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send Message")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = CreateMapForm()
+    form2 = CreateContactForm()
+    incident_list = get_incident_list()
+    if form2.validate_on_submit():
+        message = create_sns_message(form2.name.data, form2.email.data, form2.message.data)
+        publish_sns_message(topic, message)
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        m = create_incident_map(form.incident.data, form.neighborhood.data, df, geojson_df)
+        m = m._repr_html_()
+        return render_template('index.html', form=form, form2=form2, map=m,
+                               incident_list=incident_list,
+                               neighborhood_list=neighborhood_list)
+    m = create_incident_map('All Incidents', 'Entire City', df, geojson_df)
+    m = m._repr_html_()
+    return render_template('index.html', form=form, form2=form2, map=m,
+                           incident_list=incident_list,
+                           neighborhood_list=neighborhood_list)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ funcs.py
@@ -0,0 +1,100 @@
+import boto3
+import pandas as pd
+import botocore
+import folium
+import json
+from shapely.geometry import shape
+
+
+def create_sns_message(name, email, message):
+    message = name + '\n' + email + '\n' + message
+    return message
+
+
+def publish_sns_message(topic, message):
+    response = topic.publish(Message=message)
+    message_id = response['MessageId']
+    return message_id
+
+
+def retrieve_72hr_dataframe(bucket, filename):
+    s3 = boto3.resource('s3')
+    try:
+        s3.Bucket(bucket).download_file(filename, 'static/df_72hr.csv')
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+    df = pd.read_csv('static/df_72hr.csv')
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    return df
+
+
+def get_incident_list():
+    with open("static/incident_list.txt", 'r') as f:
+        incident_list = [line.rstrip('\n') for line in f]
+    return incident_list
+
+
+def create_neighborhood_list(df):
+    neighborhood_list = []
+    for index, series in df.iterrows():
+        if series.city == 'Seattle':
+            neighborhood_list.append(series['name'])
+    neighborhood_list.sort(reverse=True)
+    neighborhood_list.insert(0, 'Entire City')
+    return neighborhood_list
+
+
+def create_geojson_df_csv(geojson):
+    # df = gpd.read_file(geojson)
+    # df['center_lon'] = df['geometry'].centroid.x
+    # df['center_lat'] = df['geometry'].centroid.y
+    # return df
+    with open(geojson) as f:
+        features = json.load(f)['features']
+    center_lon = [shape(feature["geometry"]).buffer(0).centroid.x for feature in features]
+    center_lat = [shape(feature["geometry"]).buffer(0).centroid.y for feature in features]
+    name = [feature['properties']['name'] for feature in features]
+    city = [feature['properties']['city'] for feature in features]
+    geometry = [feature['geometry'] for feature in features]
+    zipped = list(zip(name, city, center_lon, center_lat, geometry))
+    df = pd.DataFrame(zipped, columns=['name', 'city', 'center_lon', 'center_lat', 'geometry'])
+    return df
+
+
+create_geojson_df_csv('static/seattle_neighborhoods.geojson')
+
+
+def create_incident_map(current_incident, neighborhood, incident_df, geojson_df):
+    if neighborhood == 'Entire City':
+        m = folium.Map(location=[47.608, -122.335], zoom_start=12)
+    else:
+
+        m = folium.Map(location=[geojson_df[geojson_df.name == neighborhood].center_lat,
+                                 geojson_df[geojson_df.name == neighborhood].center_lon],
+                       zoom_start=15)
+
+        with open('static/seattle_neighborhoods.geojson') as f:
+            features = json.load(f)['features']
+        for feature in features:
+            if feature['properties']['name'] == neighborhood:
+                folium.GeoJson(feature['geometry'], name='geojson').add_to(m)
+        # print(geojson_df[geojson_df.name == neighborhood].geometry)
+        # folium.GeoJson(json.dumps(geojson_df[geojson_df.name == neighborhood].geometry, indent=4), name='geojson').add_to(m)
+    for index, series in incident_df.iterrows():
+        if current_incident == 'All Incidents':
+            folium.Marker(
+                location=[series['latitude'], series['longitude']],
+                popup=series['marker_text'],
+                icon=folium.Icon(color=series['color'], icon=series['icon'], prefix='fa'),
+            ).add_to(m)
+        else:
+            if series['incident_type'] == current_incident:
+                folium.Marker(
+                    location=[series['latitude'], series['longitude']],
+                    popup=series['marker_text'],
+                    icon=folium.Icon(color=series['color'], icon=series['icon'], prefix='fa'),
+                ).add_to(m)
+    return m
